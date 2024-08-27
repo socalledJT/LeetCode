2e8ffb402794cecfec633e2de2f1881731326a73
Saimir Brahja <36928647+socalledJT@users.noreply.github.com>
2024-08-27 22:11:10 +0200

    Create LeetCode100.py

---
+++ Python/LeetCode100.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Taking care of edge cases
+        if not p and not q: # If both trees are empty then they are technically equal
+            return True
+        if not p or not q or p.val != q.val: # If only one is empty, or the values don't match, they're not euqal
+            return False
+        # The function will call itself for each not=de to the left and to the right of the tree
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.righ

7e20da15ff720af3965c6abd4ae386f3503bdfc5
Saimir Brahja <36928647+socalledJT@users.noreply.github.com>
2025-11-06 20:39:52 +0100

    Minor bug fix

--- JAVA/src/LeetCode42/Solution.java
+++ JAVA/src/LeetCode42/Solution.java
@@ -1,4 +1,4 @@
-class Solution {
+public class Solution {
     public int trap(int[] height) {
         // Take into consideration that the array might be empty
         if (height == null || height.length == 0) {
